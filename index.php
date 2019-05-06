f1755022f1f19348075b76fadf3c9601135d2b33
louisMAURY <louismaury33@gmail.com>
2019-05-06 12:46:56 +0200

    l'arrive d'une precipitation

--- index.php
+++ index.php
@@ -5,12 +5,14 @@
 <body>
     <h1>La meteo !</h1>
 
+    <p>nous somme le : <?php echo date('j/m/y') ?></p>
+
     <p>Vous voulez la météo ?
     D'accord mais de quelle ville ?</p>
-    
+
     <ul>
         <li><a id="meteo-paris" href="paris.php" alt="lien vers la meteo de paris">Paris, la ville lumière</a></li>
-        <li><a id="meteo-bordeaux" href="bordeaux.php" alt="lien vers la meteo de bordeaux">Bordeaux, la ville du vin</a></li>   
-    </ul> 
+        <li><a id="meteo-bordeaux" href="bordeaux.php" alt="lien vers la meteo de bordeaux">Bordeaux, la ville du vin</a></li>
+    </ul>
 </body>
 </html>
--- paris.php
+++ paris.php
@@ -8,28 +8,24 @@
     <h1>Meteo de Paris</h1>
     <h2>Aujourd'hui</h2>
     <?php
-        $day1 = "https://www.prevision-meteo.ch/uploads/widget/paris_0.png";
-        print '<img src="$day1" alt="meteo pour le Jour 1 />';
+        print '<img src="https://www.prevision-meteo.ch/uploads/widget/paris_0.png" width="650" height="250" />';
     ?>
     <h2>Jour + 1</h2>
     <?php
         $day2 = "https://www.prevision-meteo.ch/uploads/widget/paris_1.png";
-        print '<img src="$day2" alt="meteo pour le Jour 2 />';
+        print '<img src="https://www.prevision-meteo.ch/uploads/widget/paris_1.png" width="650" height="250" />';
     ?>
     <h2>Jour + 2</h2>
     <?php
-        $day3 = "https://www.prevision-meteo.ch/uploads/widget/paris_2.png";
-        print '<img src="$day3" alt="meteo pour le Jour 3 />';
+        print '<img src="https://www.prevision-meteo.ch/uploads/widget/paris_2.png" width="650" height="250" />';
     ?>
     <h2>Jour + 3</h2>
     <?php
-        $day4 = "https://www.prevision-meteo.ch/uploads/widget/paris_3.png";
-        print '<img src="$day4" alt="meteo pour le Jour 4 />';
+        print '<img src="https://www.prevision-meteo.ch/uploads/widget/paris_3.png" width="650" height="250" />';
     ?>
     <h2>Jour + 4</h2>
     <?php
-        $day5 = "https://www.prevision-meteo.ch/uploads/widget/paris_0.png";
-        print '<img src="$day5" alt="meteo pour le Jour 5 />';
+        print '<img src="https://www.prevision-meteo.ch/uploads/widget/paris_4.png" width="650" height="250" />';
     ?>
 </body>
 </html>
